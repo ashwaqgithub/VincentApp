ec4ffe270c8bdd39627fb2f8c789cd7b4ad591c5
shoq <shoq.dh@gmail.com>
2020-05-19 05:51:43 +0300

    second version

--- VincentApp/SignUpView.swift
+++ VincentApp/SignUpView.swift
@@ -9,53 +9,96 @@
 import SwiftUI
 
 struct SignUpView: View {
-    @Binding var em :String
-    @Binding var pass :String
+    @State var user: AshwaqUser = AshwaqUser()
+    @State var password: String = ""
+    @State var uid: String = ""
+    @State var userType: String = ""
+    @State var signedUp = false
+    @State var signedUpFailed = false
     var body: some View {
-
-        VStack{
-        TextField("Enter your Email", text: $em)
-              .textFieldStyle(RoundedBorderTextFieldStyle())
-              .frame(width: 300, alignment: .leading)
-              .offset( y: -20)
-          
-          TextField("Enter your password", text: $pass)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-              .frame(width: 300,alignment: .leading)
-               .offset( y: -10)
-
-
- TextField("Enter your Email", text: $em)
-              .textFieldStyle(RoundedBorderTextFieldStyle())
-              .frame(width: 300, alignment: .leading)
-//              .offset( y: -20)
-          
-          TextField("Enter your password", text: $pass)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-              .frame(width: 300,alignment: .leading)
-//               .offset( y: -10)
+        ZStack{
+            Color("BGColor")
             
-         TextField("Confirm password", text: $pass)
-              .textFieldStyle(RoundedBorderTextFieldStyle())
-                              
+            
+            
+            VStack( alignment: .center){
+                Image("vincent")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Capsule())
+                    .overlay(Circle().stroke(Color.black,lineWidth: 2))
+                    .shadow(radius: 0.9)
+                    .frame(width: 200, height: 200 , alignment: .center)
+                   
+                     .padding(.top)
+                VStack( alignment: .leading) {
+                TextField("Enter your Email", text: $user.email )
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300, alignment: .leading)
+                    .padding(.top)
+                
+                TextField("Enter your password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
                     .frame(width: 300,alignment: .leading)
-            TextField("User type", text: $pass)
-                   .textFieldStyle(RoundedBorderTextFieldStyle())
-                                   
-                         .frame(width: 300,alignment: .leading)
-                         
-            
- 
+                    .padding(.top)
+                
+                
+                Text("Select your User Type:")
+                    .font(.system(size:25))
+                    
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                    .padding(.top)
+                HStack{
+                    Text("Buyer")
+                    .font(.system(size:20))
+                                       
+                    .bold()
+                    //                .onTapGesture() {
+                    //                    userType = $user.userType
+                    //                }
+                    Text("Seller")
+                    .font(.system(size:20))
+                    .bold()
+                    //                .onTapGesture() {
+                    //                    userType =  $user.userType
+                    //                }
+                    
+                    
+                    //
+                    
+                }.padding()
+                }
+                
+                    VStack(alignment: .center, spacing: 5) {
+                    Button(action: {
+                                               //
+                                           }) {
+                                               
+                                               Text("Register")
+                                                   .font(.custom("Helvetica Neue", size: 27))
+                                                   .fontWeight(.bold)
 
-        }
-
+                                                   .frame(width:170 ,height:50, alignment: .center)
+                                                   .background(Color("SignupColor"))
+                                                   .clipShape(Capsule())
+                                                   .foregroundColor(.white)
+                                    
+                                           }.padding(90)
+                
+                    
+                
+                
+            }
+                
+            }
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
-//struct SignUpView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        SignUpView()
-//    }
-//}
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
--- VincentApp/User.swift
+++ VincentApp/User.swift
@@ -5,12 +5,58 @@
 //  Created by Eng.shoqi aldh on 5/16/20.
 //  Copyright © 2020 Eng.shoqi aldh. All rights reserved.
 //
+//
+//import Foundation
+//struct User{
+//    var email: String
+//  var password: String
+//    var type: String
+//    var category: Categor
+//}
+//enum Categor {
+//    case drawing,painting
+//}
 
-import Foundation
-struct User{
-    var email: String
-  var password: String
-    var type: String
+    import Foundation
+//    import FirebaseFirestore
+//    import class Firebase.User
+//    typealias FirebaseUser = Firebase.User
+
+
+    /// DONT edit this part
+    protocol User: Codable{
+        var uid: String? {get set}
+        var firstname: String {get set}
+        var lastname: String {get set}
+        var email: String {get set}
+        var phoneNumber: String {get set}
+        var userType: UserType {get set}
+    }
+
+    extension User{
+        func fullName() -> String{
+            firstname + " " + lastname
+        }
+    }
+
+
+    /// You can customize your user here.
+    struct AshwaqUser: User{
+        var uid: String? = nil
+        var firstname: String = ""
+        var lastname: String = ""
+        var email: String = ""
+        var phoneNumber: String = ""
+        var userType: UserType = .Buyer
+    }
+
+
+    struct SignInCredentials: Encodable
+    {
+        var email: String
+        var password: String
+    }
+
+enum UserType : String , Codable {
+    case Buyer, Seller
 }
-
-    
